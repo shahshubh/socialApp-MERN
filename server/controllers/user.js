8ebe8a9b57f43efe2c8f24d286325419f8290eac
shubh <shahshubh1010@gmail.com>
2020-04-30 19:32:27 +0530

    .

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const User = require('../models/user');
 
 exports.userById = (req, res, next, id) => {
@@ -31,4 +33,40 @@ exports.allUsers = (req,res) => {
         }
         return res.json({ users });
     }).select("name email updated created");
+};
+
+exports.getUser = (req, res) => {
+    // set hashed_password and salt undefined since we dont want it in response while viewing single user
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+exports.updateUser = (req, res, next) => {
+    let user = req.profile;
+    // extend method from lodash => mutates the source object
+    user = _.extend(user, req.body);
+    user.updated = Date.now();
+    user.save((err) => {
+        if(err){
+            return res.status(400).json({
+                error: "You are not authorized to perform this action !!"
+            });
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({user})
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+    let user = req.profile;
+    user.remove((err, user) => {
+        if(err){
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.json({ message: "User deleted successfully" });
+    });
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,15 @@
 const express = require('express')
-const { userById, allUsers } = require('../controllers/user');
+
+const { userById, allUsers, getUser, updateUser, deleteUser } = require('../controllers/user');
+const { requireSignin } = require('../controllers/auth');
+
 
 const router = express.Router();
 
 router.get("/users", allUsers);
+router.get("/user/:userId", requireSignin, getUser);
+router.put("/user/:userId", requireSignin, updateUser);
+router.delete("/user/:userId", requireSignin, deleteUser);
 
 // any route containing :userId, this is execute first
 router.param("userId", userById);
