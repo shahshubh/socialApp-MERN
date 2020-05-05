f3d9e8a901348096600ef4399a532bc22a42a375
shubh <shahshubh1010@gmail.com>
2020-05-05 11:28:22 +0530

    server - like/unlike api routes

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,13 +1,18 @@
 const express = require('express')
 
 const { requireSignin } = require('../controllers/auth');
-const { getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost, photo, singlePost } = require('../controllers/post')
+const { getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost, photo, singlePost, like, unlike } = require('../controllers/post')
 const { userById } = require('../controllers/user');
 const {createPostValidator} = require('../validator/index');
 
 const router = express.Router();
 
 router.get('/posts', getPosts );
+
+//like unlike
+router.put("/post/like", requireSignin, like);
+router.put("/post/unlike", requireSignin, unlike);
+
 router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
 router.get("/post/by/:userId", postsByUser);
 router.get("/post/:postId", singlePost);
